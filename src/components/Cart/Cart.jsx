9b0c4cef1985c42c813d4197ef7788de9be0b4a6
CodervsFixer <codervsfixer.com>
2023-03-29 05:19:41 +0600

    Add cart price and calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,17 +4,21 @@ import './Cart.css';
 const Cart = (props) => {
     const {cart} = props;
     let total = 0;
+    let totalShipping = 0;
     for (const product of cart) {
         total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
     }
+    const tax = total*7/100;
+    const grandTotal = total+totalShipping+tax;
     return (
         <div className='cart-container'>
             <h2>Order Summery</h2>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shopping charge: </p>
-            <p>Tax: </p>
-            <h4>Grand Total: </h4>
+            <p>Total Price: ${total}</p>
+            <p>Total Shopping charge: ${totalShipping}</p>
+            <p>Tax: ${tax.toFixed(2)}</p>
+            <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
             <div>
                 <button className='clearCart'>Clear Cart</button>
                 <button className='reviewCart'>Review Order</button>
